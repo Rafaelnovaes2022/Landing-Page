3a4f4e2f82a8d3cc0964459df720d26a86f5d757
Rafaelnovaes2022 <rafael_novaes2015@hotmail.com>
2023-10-24 14:10:14 -0300

    em progresso

---
+++ src/components/HomePage.vue
@@ -0,0 +1,40 @@
+<template>    
+    <div class="container-fluid m-0 p-0">
+
+        <div id="home-page" class="full-height">
+
+        </div>
+
+        <div id="about" class="bg-warning p-4">
+
+        </div>
+
+        <div id="contact" class="bg-info p-4">
+
+        </div>
+
+        <div id="footer" class="bg-primary p-4">
+
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+
+    }
+
+}
+</script>
+
+<style scoped>
+
+#home-page {
+    background-color: #92D2F2;
+    background: url('../assets/bg-mt.jpg');
+}
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<!-- <template>
+  <div></div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomePage from '@/components/HomePage.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    HomePage
+  }
+}
+</script> -->
