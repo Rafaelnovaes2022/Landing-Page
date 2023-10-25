2d42ce1de7402e911e3a7bc61ba57c27158375a8
Rafaelnovaes2022 <rafael_novaes2015@hotmail.com>
2023-10-25 14:10:55 -0300

    landing page-configurando firebase

---
+++ src/firebase/Credentials.js
@@ -0,0 +1,22 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAlL5uO7n7uuERYvYrjMAb6L2Z2-D1Pvp8",
+    authDomain: "landig-page-aad4f.firebaseapp.com",
+    projectId: "landig-page-aad4f",
+    storageBucket: "landig-page-aad4f.appspot.com",
+    messagingSenderId: "1079431367726",
+    appId: "1:1079431367726:web:d330d13104723376f123bf",
+    measurementId: "G-37LQC2BFKX"
+  };
+
+// export default {
+//     config: {
+//       apiKey: "",
+//       authDomain: "",
+//       databaseURL: "",
+//       projectId: "",
+//       storageBucket: "",
+//       messagingSenderId: "",
+//       appId: "",
+//     }
+// }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { firestorePlugin } from 'vuefire';
+
 import '@popperjs/core';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+Vue.config.productionTip = false;
+Vue.use(firestorePlugin)
+
 
 createApp(App).use(router).mount('#app')
