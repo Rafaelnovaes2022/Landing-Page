d00c9c188bd9c59944749f9243f94429b5104ef7
Rafaelnovaes2022 <rafael_novaes2015@hotmail.com>
2023-10-24 21:07:57 -0300

    HomePage em andamento

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,23 +1,38 @@
-<template>    
+<template>
     <div class="container-fluid m-0 p-0">
 
-        <div id="home-page" class="full-height">
+        <div id="home-page" class="full-height p-4">
+            <div class="container pt-3">
+
+                <div class="row pb-5">
+                    <div class="col-lg-6">
+                        <img if="iphone-img" src="../assets/travelTreasuryiPhone.png" class="img-fluig">
+
+                    </div>
+
+                    <div class="col-lg-6">
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div id="about" class="bg-warning p-4">
+
+            </div>
+
+            <div id="contact" class="bg-info p-4">
+
+            </div>
+
+            <div id="footer" class="bg-primary p-4">
+
+            </div>
 
         </div>
-
-        <div id="about" class="bg-warning p-4">
-
-        </div>
-
-        <div id="contact" class="bg-info p-4">
-
-        </div>
-
-        <div id="footer" class="bg-primary p-4">
-
-        </div>
-        
-    </div>
 </template>
 
 <script>
@@ -31,10 +46,8 @@ export default {
 </script>
 
 <style scoped>
-
 #home-page {
     background-color: #92D2F2;
-    background: url('../assets/bg-mt.jpg');
+    background: url('../assets/bg_mt.jpg');
 }
-
 </style>
