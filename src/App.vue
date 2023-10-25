b3d4e6463f8864d0093d65a9fcafce6d193f0239
Rafaelnovaes2022 <rafael_novaes2015@hotmail.com>
2023-10-25 11:16:58 -0300

    HomePage em andamento

--- src/App.vue
+++ src/App.vue
@@ -11,28 +11,16 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 
-.full-height {
-  min-height: 100vh;
-}
-// nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
+// .full-height {
+//   min-height: 100vh;
 // }
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,14 +5,59 @@
             <div class="container pt-3">
 
                 <div class="row pb-5">
-                    <div class="col-lg-6">
-                        <img if="iphone-img" src="../assets/travelTreasuryiPhone.png" class="img-fluig">
+                    <div class="col-lg-6 pt-4 my-auto">
+                        <div class="text-center mb-3 d-block d-lg-none">
+                            <h1 class="display-3">{{ title }}</h1>
+                            <h3>Find the World</h3>
+                        </div>
+
+                        <img id="iphone-img" src="../assets/travelTreasuryiPhone.png" class="img-fluig">
 
                     </div>
 
-                    <div class="col-lg-6">
+                    <div class="col-lg-6 my-auto pt-4">
+                        <div class="text-center mb-3 d-none d-lg-block">
+                            <h1 class="display-3">{{ title }}</h1>
+                            <h3>Find the World</h3>
+                        </div>
 
-                    </div>
+                        <div class="border p-3 p-md-5 bg-white rounded shadow">
+                            <h2 class="text-center">Coming Soon</h2>
+                            <form>
+                                <div class="form-group">
+                                    <label for="emailSignup">Reserve your account now, we'll contact you when it's
+                                        live</label>
+                                    <input id="emailSignup" placeholder="Enter Email" class="form-control">
+                                    <div class="text-center">
+                                        <small id="emailHelp" class="form-text text-muted" style="text-align: center;">
+                                            We'll never share your email address
+                                        </small>
+                                    </div>
+
+                                </div>
+
+                                <div class="text-center mt-3">
+                                    <button type="submit" class="btn btn-success">Join Waiting List</button>
+                                </div>
+                            </form>
+                        </div>
+
+
+
+
+                        <div id="available" class="p-2">
+                            <h4 class="text-center mb2 mt-3 text-light">
+                                Available On
+                            </h4>
+                            <div class="row">
+                                <div class="col my-auto">
+                                    <img src="../assets/download_apple.png" class="float-right img-fluid app_store-img">
+                                </div>
+                                <div class="col my-auto">
+                                    <img src="../assets/download_google.png" class="float-left img-fluid app_store-img">
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
@@ -20,28 +65,98 @@
 
             </div>
 
-            <div id="about" class="bg-warning p-4">
-
+        </div>
+        <div id="about" class="bg-light p-3 p-md-5">
+            <div class="row">
+                <div class="col-lg">
+                    <!-- Icon made by Freepik perfect from www.flaticon.com -->
+                    <img src="../assets/icons/app.svg" height="120" class="m-4">
+                    <h5 class="ml-md-4 mr-md-4">
+                        Plan your next adventure by setting a daily spending budget.
+                        We make it easy to break your expenses down by category,
+                        giving you more time to plan the important tings!
+                    </h5>
+                </div>
+                <div class="col-lg">
+                    <!-- Icon made by Freepik perfect from www.flaticon.com -->
+                    <img src="../assets/icons/piggy-bank.svg" height="120" class="m-4">
+                    <h5 class="ml-md-4 mr-md-4">
+                        Use the app to track how much money you’ve saved towards your trips
+                        total budgeted goal. Know exactly how much more you need to save,
+                        and how much time you have left to save.
+                    </h5>
+                </div>
+                <div class="col-lg">
+                    <!-- Icon made by Freepik perfect from www.flaticon.com -->
+                    <img src="../assets/icons/passport.svg" height="120" class="m-4">
+                    <h5 class="ml-md-4 mr-md-4">
+                        When the day of your trip comes, have the peace of mind that you
+                        planned out your expenses. Never worry about money on a trip again!
+                    </h5>
+                </div>
             </div>
+        </div>
 
-            <div id="contact" class="bg-info p-4">
+        <div id="contact" class="p-4">
+            <div class="row justify-content-center mt-3 mb-3">
 
-            </div>
+                <div v-if="show_contact == true" class="col-lg-4">
+                    <h2>Have Any Questions?</h2>
+                    <p>Contact us by filling out the information below</p>
 
-            <div id="footer" class="bg-primary p-4">
+                    <div v-if="contact_notice != ''" class="alert alert-warning">
+                        There was a problem submitting your message. {{ contact_notice }}
+                    </div>
 
+                    <form @submit.prevent="sendContactMessage()">
+                        <div class="form-group text-left">
+                            <input v-model="contact_email" type="email" class="form-control" placeholder="Enter Your Email">
+                            <textarea v-model="contact_message" class="form-control mt-3" placeholder="Enter Your Message"
+                                rows="5"></textarea>
+                        </div>
+                        <div class="text-center"> <!-- Adicione esta div para centralizar -->
+                            <button type="submit" class="btn btn-primary m-3">Send Message</button>
+                        </div>
+                    </form>
+
+                </div>
+
+                <div v-else>
+                    <h3>Message Sent Successfully!</h3>
+                    <p>Thank you for contacting us, we'll get back to you as soon as we can.</p>
+                </div>
             </div>
 
         </div>
+
+        <div id="footer" class="p-4">
+            <footer class="text-light p-4">
+                <a href="https://www.youtube.com/1manstartup" target="_blank" class="text-warning">Build This App</a> |
+                <a href="#" class="text-warning">Privacy Policy</a>
+                <br>
+                <small>&copy; 2020, TravelTreasury.app</small>
+            </footer>
+        </div>
+
+
+    </div>
 </template>
+
 
 <script>
 
 export default {
     data() {
-
-    }
-
+        return {
+            title: 'Travel Treasury',
+            email: '',
+            message: '',
+            show_contact: true,
+            contact_email: '',
+            contact_message: '',
+            contact_notice: '',
+        }
+    },
 }
 </script>
 
@@ -49,5 +164,29 @@ export default {
 #home-page {
     background-color: #92D2F2;
     background: url('../assets/bg_mt.jpg');
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+
+#about {
+    min-height: 40vh;
+}
+
+#iphone-img {
+    max-height: 80vh;
+}
+
+#contact {
+    background-color: #57AEAF;
+}
+
+#footer {
+    background-color: #252223;
+}
+
+.app_store_img {
+    max-height: 80px;
 }
 </style>
